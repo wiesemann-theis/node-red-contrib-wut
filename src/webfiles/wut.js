fce6380bc60b6df2d467e6b27e09d3efca8a35d8
tt92 <tobi-theis@gmx.de>
2019-08-02 15:28:55 +0200

    resolved redundancies

--- src/webfiles/wut.js
+++ src/webfiles/wut.js
@@ -1,5 +1,15 @@
 (function ($, window) {
-    function prepareWebioClamp(clampNumber, isAnalog, defaultClampCount, labels, ranges) {
+    /********** helper functions ****************/
+    function sanitize(htmlText) {
+        return $('<div>' + (htmlText || '').replace(/<br[ \/]*>/g, ' ') + '</div>').text();
+    }
+    function onKeyDown(e) {
+        if (e.keyCode === 13 && $(e.target).hasClass('select2-search__field')) {
+            e.stopPropagation(); // blame: for some reason, this event causes an error in Node-RED core when propagated to window...
+        }
+    }
+
+    function initClampConfig(clampNumber, isAnalog, defaultClampCount, labels, ranges) {
         let lastWebio = null;
         let lastNumber = clampNumber;
 
@@ -44,7 +54,165 @@
         });
     }
 
+    function initDeviceConfig(node) {
+        let isDiscovering = false;
+        let isManualConfig = node.manualConfig;
+        let lastDeviceId = node.device;
+        let allDevices = [];
+
+        const isWebio = node.type === 'Web-IO';
+        const idPrefix = isWebio ? '#node-config-input-' : '#node-input-';
+        const type = isWebio ? 'webio' : 'comserver';
+
+        const discoverDevices = function (clearCache) {
+            isDiscovering = true;
+            $('#btn-discover-devices').prop('disabled', true);
+            $('#btn-discover-devices > i').attr('class', 'fa fa-fw fa-spinner fa-spin');
+            const url = 'wut/devices/' + type + '?nodeId=' + node.id + (clearCache ? '&clearCache=true' : '');
+            $.getJSON(url, function (data) {
+                isDiscovering = false;
+                $('#btn-discover-devices > i').attr('class', 'fa fa-fw fa-search');
+                $('#btn-discover-devices').prop('disabled', isManualConfig || isDiscovering);
+                if (data) {
+                    allDevices = data;
+                    $(idPrefix + 'device').children('option').remove(); // remove all clamp options
+
+                    for (let i = 0; i < data.length; ++i) {
+                        const device = data[i];
+                        device.text = device.ip + (isWebio ? ':' + device.port : '') + ' (' + device.mac + ')';
+                        $(idPrefix + 'device').append($('<option></option>').attr('value', device.id).text(device.text).attr('data-data', JSON.stringify(device)));
+                    }
+
+                    if (!lastDeviceId) {
+                        const matches = allDevices.filter(function (d) {
+                            let match = d.ip === node.host;
+                            if (isWebio) {
+                                match &= +d.port === +node.port && node.protocol === (d.httpsEnabled ? 'https' : 'http');
+                            }
+                            return match;
+                        });
+                        lastDeviceId = (matches[0] || {}).id;
+                    }
+                    $(idPrefix + 'device').val(lastDeviceId).trigger('change'); // finally, restore selection
+                }
+            });
+        };
+
+        $(idPrefix + 'device').change(function () {
+            const selectedId = $(this).val();
+            lastDeviceId = selectedId || lastDeviceId;
+            const device = allDevices.filter(function (d) { return d.id === selectedId; })[0];
+            if (device) { // set host, protocol and port values
+                $(idPrefix + 'host').val(device.ip || '').trigger('change');
+                if (isWebio) {
+                    $(idPrefix + 'protocol').val(device.httpsEnabled ? 'https' : 'http').trigger('change');
+                    $(idPrefix + 'port').val(device.port || '').trigger('change');
+                }
+            }
+            if (device && !isManualConfig) { // set product details
+                $('#device-details-wrapper').removeClass('hidden');
+                const productId = device.productId || '';
+                $('#device-details-productid').text(productId).attr('href', 'https://www.wut.de/' + productId);
+                $('#device-details-prodname').text(sanitize(device.prodname));
+                $('#device-details-sysname').text(sanitize(device.sysname));
+            } else {
+                $('#device-details-wrapper').addClass('hidden');
+            }
+        });
+
+        $(idPrefix + 'manualConfig').change(function () {
+            isManualConfig = this.checked;
+            $('.manual-config-input').prop('disabled', !isManualConfig);
+            $(idPrefix + 'device').prop('disabled', isManualConfig);
+            $('#btn-discover-devices').prop('disabled', isManualConfig || isDiscovering);
+            if (!isManualConfig) {
+                $(idPrefix + 'device').trigger('change');
+            } else {
+                $('#device-details-wrapper').addClass('hidden');
+            }
+        });
+
+        $('#btn-discover-devices').click(function (e) {
+            e.preventDefault();
+            discoverDevices(true); // re-discover devices (clear cache!)
+        });
+
+        $('#btn-webconfig').click(function (e) {
+            e.preventDefault();
+            const protocol = isWebio ? $(idPrefix + 'protocol').val() : 'http';
+            const host = $(idPrefix + 'host').val();
+            const port = isWebio ? $(idPrefix + 'port').val() : 80;
+            if (protocol && host && port) {
+                window.open(protocol + '://' + host + ':' + port, '_blank');
+            }
+        });
+
+        if ($(window).select2) {
+            $('body').on('keydown', onKeyDown);
+            $('body').on('keydown', '.select2.select2-container', function (e) {
+                const KEYS = { UP: 38, DOWN: 40 };
+                if (e.keyCode === KEYS.UP || (e.keyCode === KEYS.DOWN && !e.altKey)) {
+                    e.preventDefault();
+                    const select = $(this).siblings("select.wut-select2");
+                    if (select && select.length) {
+                        let newValue;
+                        if (e.keyCode === KEYS.UP) {
+                            newValue = select.find('option:selected').prevAll(":enabled").first().val();
+                        } else {
+                            newValue = select.find('option:selected').nextAll(":enabled").first().val();
+                        }
+
+                        if (newValue) {
+                            select.val(newValue).trigger('change');
+                        }
+                    }
+                }
+            });
+            setTimeout(function () {
+                $('select.wut-select2:not(' + idPrefix + 'device)').select2();
+                $(idPrefix + 'device').select2({
+                    templateResult: function (device) {
+                        let template = '<div class="wut-select2-dropdown"><div class="wut-row">' + device.text + '</div>'
+                        let title = device.text;
+                        if (device.prodname) {
+                            if (device.productId) {
+                                template += '<div style="display: flex;"><small class="wut-row">' + sanitize(device.prodname) + '</small><small>&nbsp;(#' + device.productId + ')</small></div>';
+                                title += '\n' + sanitize(device.prodname) + ' (#' + device.productId + ')';
+                            } else {
+                                template += '<div class="wut-row"><small>' + sanitize(device.prodname) + '</small></div>';
+                                title += '\n' + sanitize(device.prodname);
+                            }
+                        } else if (device.productId) {
+                            template += '<div class="wut-row"><small>' + node._('config.label.productid') + ' #' + device.productId + '</small></div>';
+                            title += '\n' + node._('config.label.productid') + ' #' + device.productId;
+                        }
+                        if (device.sysname) {
+                            template += '<div class="wut-row"><small>' + sanitize(device.sysname) + '</small></div>';
+                            title += '\n' + sanitize(device.sysname);
+                        }
+                        template += '</div>';
+                        return $(template).attr('title', title);
+                    },
+                    matcher: function (params, data) {
+                        const searchBase = [data.text, data.prodname, data.sysname, data.productId].filter(Boolean).join('   ').toLowerCase();
+                        const searchString = $.trim(params.term).toLowerCase();
+                        return searchBase.indexOf(searchString) > -1 ? data : null;
+                    }
+                });
+            }, 500);
+        } else {
+            console.warn('select2 is not available');
+        }
+
+        discoverDevices(false); // initially discover devices (cached data is ok)
+    }
+
+    function removeSelect2EventHandler() {
+        $('body').off('keydown', onKeyDown);
+        $('body').off('keydown', '.select2.select2-container');
+    }
+
     $(document).ready(function () {
-        window.wut = { prepareWebioClamp };
+        window.wut = { initClampConfig, initDeviceConfig, removeSelect2EventHandler };
     });
 })(jQuery, window);
