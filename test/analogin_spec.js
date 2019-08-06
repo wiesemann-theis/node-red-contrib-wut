c2f006b04b7ec6f4daf5962c6e45dedbbc8bcebe
tt92 <tobi-theis@gmx.de>
2019-08-06 15:04:02 +0200

    unit testing first steps

---
+++ test/analogin_spec.js
@@ -0,0 +1,26 @@
+const should = require("should");
+const helper = require("node-red-node-test-helper");
+const analogInNode = require("../src/analogin.js");
+
+helper.init(require.resolve('node-red'));
+
+describe('Analog IN Node', function () {
+
+  beforeEach(function (done) {
+    helper.startServer(done);
+  });
+
+  afterEach(function (done) {
+    helper.unload();
+    helper.stopServer(done);
+  });
+
+  it('should be loaded', function (done) {
+    const flow = [{ id: "n1", type: "Analog IN", name: "Demo Analog IN" }];
+    helper.load(analogInNode, flow, function () {
+      const n1 = helper.getNode("n1");
+      n1.should.have.property('name', 'Demo Analog IN');
+      done();
+    });
+  });
+});
